33e347822ea2a188d91c96039347fb61421965e3
richasinha2 <92218076+richasinha2@users.noreply.github.com>
2024-11-22 23:32:02 +0800

    Create insert_sample_data.sql

---
+++ sql/insert_sample_data.sql
@@ -0,0 +1,5 @@
+INSERT INTO Customers (First_Name, Last_Name, Email_Office, Email_Personal, Family_Members)
+VALUES ('John', 'Doe', 'john.doe@office.com', 'john.doe@gmail.com', 'Jane, Jack');
+
+INSERT INTO Products (Book_Title, Book_Price, Book_Quantity)
+VALUES ('Oracle PL/SQL Programming', 29.99, 100);
